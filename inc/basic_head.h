fee6c0734b39ebc6d96b57f8ed7842ff1cdb50b3
rjf12138 <3161173206@qq.com>
2020-10-28 07:14:51 +0800

    first commit

---
+++ inc/basic_head.h
@@ -0,0 +1,39 @@
+#ifndef __BASIC_HEAD_H__
+#define __BASIC_HEAD_H__
+
+/////////////// c++ header file //////////////////////
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cstdarg>
+#include <map>
+#include <memory>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+
+#ifdef __gnu_linux__ 
+////////////// linux system header file ////////////////
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#endif
+////////////// defined by myself ////////////////////////
+
+#endif
